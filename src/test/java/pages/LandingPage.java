0b26c207a8291a104882a8b0dbdbf73e26908972
chandanashree7 <122770911+chandanashree7@users.noreply.github.com>
2023-02-24 13:50:38 -0500

    Update LandingPage.java
    "removed wrong  import "

--- src/test/java/pages/LandingPage.java
+++ src/test/java/pages/LandingPage.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import rahulshettyacademy.pageobjects.CheckoutPage;
 
 public class LandingPage {
 	
